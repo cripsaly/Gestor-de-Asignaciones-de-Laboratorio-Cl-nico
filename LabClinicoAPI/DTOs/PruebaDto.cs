911eed0b946b7d81b60e86e8cadd439356c8a228
cripsaly <20250946@itla.edu.do>
2026-06-13 20:56:14 -0400

    Delete LabClinicoAPI directory

--- LabClinicoAPI/DTOs/PruebaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LabClinicoAPI.DTOs
-{
-    public class PruebaDto
-    {
-        public int IdPrueba { get; set; }
-        public string NombrePrueba { get; set; }
-        public string? Descripcion { get; set; }
-    }
-}
